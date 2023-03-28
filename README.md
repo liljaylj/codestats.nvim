df22157db1d06880c79b90d6c0009d9075f90387
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2023-03-28 13:10:46 +0600

    feat: validate config

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ This is port of the [official plugin](https://gitlab.com/code-stats/code-stats-v
       api_key = '<your API key>',
       send_on_exit = true,  -- send xp on nvim exit
       send_on_timer = true,  -- send xp on timer
-      timer_interval = 10000,  -- timer interval in milliseconds
+      timer_interval = 60000,  -- timer interval in milliseconds (minimum 1000ms to prevent DDoSing codestat.net servers)
       curl_timeout = 5,  -- curl request timeout in seconds
     }
   end,
--- lua/codestats/init.lua
+++ lua/codestats/init.lua
@@ -14,16 +14,41 @@ local filetype_to_language = function(ft)
 end
 
 local CodeStats = {
+  config = {
+    base_url = 'https://codestats.net', -- codestats.net base url
+    send_on_exit = true, -- send xp on nvim exit
+    send_on_timer = true, -- send xp on timer
+    timer_interval = 60000, -- timer interval in milliseconds
+    curl_timeout = 5, -- curl request timeout in seconds
+  },
+
+  current_xp_dict = {},
+
+  profile = {},
+
   setup = function(self, config)
     -- init
-    self.current_xp_dict = {}
-    self.username = config.username
-    if self.username then
-      self.profile_url = (config.base_url or 'https://codestats.net') .. '/api/users/' .. self.username
+
+    vim.validate {
+      base_url = { config.base_url, 's', true },
+      send_on_exit = { config.send_on_exit, 'b', true },
+      send_on_timer = { config.send_on_timer, 'b', true },
+      timer_interval = { config.timer_interval, 'n', true },
+      curl_timeout = { config.curl_timeout, 'n', true },
+      api_key = { config.api_key, 's' },
+    }
+    self.config = vim.tbl_deep_extend('force', self.config, config)
+    -- minimum timer timeout
+    if self.config.timer_interval < 1000 then
+      self.config.timer_interval = 1000
     end
-    self.pulse_url = (config.base_url or 'https://codestats.net') .. '/api/my/pulses'
-    self.curl_timeout = config.curl_timeout or 5
-    self.api_key = config.api_key
+
+    if self.config.username then
+      self.config.profile_url = (self.config.base_url or 'https://codestats.net')
+        .. '/api/users/'
+        .. self.config.username
+    end
+    self.config.pulse_url = (self.config.base_url or 'https://codestats.net') .. '/api/my/pulses'
 
     -- autocmds
     local group = vim.api.nvim_create_augroup('codestats-plugin-autocommands', { clear = true })
@@ -51,7 +76,7 @@ local CodeStats = {
     })
 
     -- send xp on vim leave
-    if config.send_on_exit == nil or config.send_on_exit then -- by default send xp on nvim exit
+    if self.config.send_on_exit == nil or self.config.send_on_exit then -- by default send xp on nvim exit
       vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
         group = group,
         pattern = '*',
@@ -65,11 +90,10 @@ local CodeStats = {
     end
 
     -- send xp on timer
-    if config.send_on_timer == nil or config.send_on_timer then -- by default send xp on timer
-      local interval = config.timer_interval or 60000 -- every 60 seconds
+    if self.config.send_on_timer == nil or self.config.send_on_timer then -- by default send xp on timer
       vim.loop.new_timer():start(
-        interval,
-        interval,
+        self.config.timer_interval,
+        self.config.timer_interval,
         vim.schedule_wrap(function()
           self:send_xp()
         end)
@@ -115,7 +139,7 @@ local CodeStats = {
     end
 
     curl.post {
-      url = self.pulse_url,
+      url = self.config.pulse_url,
       body = vim.json.encode {
         coded_at = os.date '%FT%T%z',
         xps = xp_list,
@@ -123,10 +147,10 @@ local CodeStats = {
       headers = {
         ['Content-Type'] = 'application/json',
         ['User-Agent'] = 'code-stats-nvim/' .. VERSION,
-        ['X-API-Token'] = self.api_key,
+        ['X-API-Token'] = self.config.api_key,
         ['Accept'] = '*/*',
       },
-      raw = { '-m', self.curl_timeout },
+      raw = { '-m', self.config.curl_timeout },
       callback = function()
         self.current_xp_dict = {}
         vim.schedule(function()
@@ -138,29 +162,34 @@ local CodeStats = {
         -- TODO: handle error
       end,
     }
-
-    -- update profile data
   end,
 
   update_profile = function(self)
-    if not self.profile_url then
+    if not self.config.profile_url then
       return
     end
     curl.get {
-      url = self.profile_url,
+      url = self.config.profile_url,
       headers = {
         ['Content-Type'] = 'application/json',
         ['User-Agent'] = 'code-stats-nvim/' .. VERSION,
-        ['X-API-Token'] = self.api_key,
+        ['X-API-Token'] = self.config.api_key,
         ['Accept'] = '*/*',
       },
-      raw = { '-m', self.curl_timeout },
+      raw = { '-m', self.config.curl_timeout },
       callback = function(result)
         local json = vim.json.decode(result.body)
-        self.total_xp = json.total_xp
-        self.new_xp = json.new_xp
-        self.total_xp_dict = json.languages
-        self.level = calculate_level(json.total_xp)
+        pcall(vim.validate, {
+          user = { json.user, 's' },
+          total_xp = { json.total_xp, 'n' },
+          new_xp = { json.new_xp, 'n' },
+          languages = { json.languages, 'table' },
+        })
+        self.profile.user = json.user
+        self.profile.total_xp = json.total_xp
+        self.profile.new_xp = json.new_xp
+        self.profile.languages = json.languages
+        self.profile.level = calculate_level(json.total_xp)
         vim.schedule(function()
           vim.api.nvim_exec_autocmds('User', { pattern = 'CodeStatsProfileUpdated', data = self })
         end)
@@ -176,13 +205,13 @@ local get_xp = function(buf)
   if not buf then
     return CodeStats.total_xp or 0
   else
-    if type(CodeStats.total_xp_dict) ~= 'table' then
+    if type(CodeStats.languages) ~= 'table' then
       return 0
     end
     local filetype = vim.api.nvim_buf_get_option(buf, 'filetype')
     local language_type = filetype_to_language(filetype)
-    local xp = (CodeStats.total_xp_dict[filetype] and CodeStats.total_xp_dict[filetype].xps) or 0
-    return xp + ((CodeStats.total_xp_dict[language_type] and CodeStats.total_xp_dict[language_type].xps) or 0)
+    local xp = (CodeStats.languages[filetype] and CodeStats.languages[filetype].xps) or 0
+    return xp + ((CodeStats.languages[language_type] and CodeStats.languages[language_type].xps) or 0)
   end
 end
 
